25b32adcc2c6ef13c72ee24bd8237a14f080701e
anupkrbid <anupbid94@gmail.com>
2018-06-30 18:33:59 +0530

    subject emited multiple times

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Observable, Subscription } from 'rxjs';
-import { take } from 'rxjs/operators';
+import { take, tap, filter, combineLatest, distinctUntilChanged } from 'rxjs/operators';
 
 import { AppService } from './app.service';
 import { Round, Question, WordVault } from './word-vault.interface';
@@ -15,59 +15,91 @@ import { VocabularyState } from './vocabulary-state.interface';
 export class AppComponent implements OnInit, OnDestroy {
 
   vault$: Observable<WordVault>;
-  vaultSubscription: Subscription;
-  rounds: { [key: string]: Round };
-  vaultState$: Observable<VocabularyState>;
-  vaultStateData: VocabularyState;
-  vaultStateSubscription: Subscription;
+  vocabularyState$: Observable<VocabularyState>;
   @ViewChild('form') form: NgForm;
+  // vaultSubscription: Subscription;
+  // rounds: { [key: string]: Round };
+  // vaultStateData: VocabularyState;
+  // vocabulaSubscription: Subscription;
 
   constructor(private appService: AppService) {}
 
   ngOnInit() {
     this.vault$ = this.appService.getWordVault();
-    this.vault$.pipe(take(1)).subscribe(data => this.rounds = data.rounds);
-    this.vaultState$ = this.appService.vocabularyState;
-    this.vaultStateSubscription = this.vaultState$.subscribe(state => this.vaultStateData = state);
+    this.vocabularyState$ = this.appService.vocabularyState$;
+    this.observeVocabularyStateChange();
+    // this.vault$.pipe(take(1)).subscribe(data => (this.rounds = data.rounds));
+    // this.vaultStateSubscription = this.vocabularyState$.subscribe(
+    //   state => (this.vaultStateData = state)
+    // );
   }
 
-  onSubmit(form: NgForm, question: Question, indexOfQuestionJustAnswered: number) {
+  onSubmit(
+    form: NgForm,
+    question: Question,
+    indexOfQuestionJustAnswered: number
+  ) {
     const answer = form.value.answer;
     const answers = question.answers;
     const correctAnswer = question.correctAnswer;
 
     if (answers[correctAnswer - 1] === answer) {
       alert('Correct answer!');
-      let newState: VocabularyState;
-      const noOfRounds = Object.keys(this.rounds).length;
-      const noOfQuestionsInCurrentRound = this.rounds[this.vaultStateData.currentRound.toString()].questions.length;
-
-      if (noOfQuestionsInCurrentRound > indexOfQuestionJustAnswered + 1) {
-        newState = {
-          currentQuestion: this.vaultStateData.currentQuestion + 1,
-          currentRound: this.vaultStateData.currentRound
-        };
-      } else {
-        newState = {
-          currentQuestion: 1,
-          currentRound: this.vaultStateData.currentRound + 1
-        };
-      }
-
-      if (newState.currentRound > noOfRounds) {
-        alert('Completed');
-      } else {
-        this.appService.vocabularyState.next(newState);
-      }
-
+      this.appService.changeVocabularyState$.next({
+        indexOfQuestionJustAnswered
+      });
     } else {
       alert('Try Again!');
       this.form.reset();
     }
   }
 
-  ngOnDestroy() {
-    this.vaultStateSubscription.unsubscribe();
+  observeVocabularyStateChange() {
+    return this.appService.changeVocabularyState$
+      .pipe(
+        take(1),
+        combineLatest(this.vault$, this.vocabularyState$),
+        tap(([changeVocabularyState, vault, vocabularyState]) =>
+          this.handleVocabularStateChange(
+            vocabularyState,
+            changeVocabularyState,
+            vault
+          )
+        )
+      )
+      .subscribe(() => console.log('observed'));
   }
 
+  handleVocabularStateChange(vocabularyState, changeVocabularyState, vault) {
+    console.log(vocabularyState);
+    let newState: VocabularyState;
+    const noOfRounds = Object.keys(vault.rounds).length;
+    const noOfQuestionsInCurrentRound = vault.rounds[
+      vocabularyState.currentRound.toString()
+    ].questions.length;
+    const questionNoJustAnswered = changeVocabularyState.indexOfQuestionJustAnswered + 1;
+
+    if (noOfQuestionsInCurrentRound > questionNoJustAnswered) {
+      newState = {
+        currentQuestion: vocabularyState.currentQuestion + 1,
+        currentRound: vocabularyState.currentRound
+      };
+    } else {
+      newState = {
+        currentQuestion: 1,
+        currentRound: vocabularyState.currentRound + 1
+      };
+    }
+
+    if (newState.currentRound > noOfRounds) {
+      alert('Completed');
+    } else {
+      this.appService.vocabularyState$.next(newState);
+    }
+
+  }
+
+  ngOnDestroy() {
+    // this.vaultStateSubscription.unsubscribe();
+  }
 }
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,4 +1,4 @@
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
@@ -12,7 +12,8 @@ import { WordVault } from './word-vault.interface';
 })
 export class AppService {
 
-  vocabularyState = new BehaviorSubject<VocabularyState>({ currentRound: 1, currentQuestion: 1 });
+  vocabularyState$ = new BehaviorSubject<VocabularyState>({ currentRound: 1, currentQuestion: 1 });
+  changeVocabularyState$ = new Subject<{ indexOfQuestionJustAnswered: number }>();
 
   constructor(private httpClient: HttpClient) {}
 

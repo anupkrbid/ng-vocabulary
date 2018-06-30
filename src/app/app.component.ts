6bf1e2dc21cfc1c6b702134bd1ad610e83a9b173
anupkrbid <anupbid94@gmail.com>
2018-06-30 23:48:07 +0530

    Refactored Code, Time to Animate Sprite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,10 @@
 import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Observable, Subscription } from 'rxjs';
-import {
-  take,
-  tap,
-  filter,
-  pairwise,
-  map,
-  combineLatest,
-  distinctUntilChanged,
-  distinctUntilKeyChanged,
-  withLatestFrom
-} from 'rxjs/operators';
-import { of } from 'rxjs';
+import { tap, withLatestFrom } from 'rxjs/operators';
 
 import { AppService } from './app.service';
-import { Round, Question, WordVault } from './word-vault.interface';
+import { Question, WordVault } from './word-vault.interface';
 import { VocabularyState } from './vocabulary-state.interface';
 
 @Component({
@@ -26,11 +15,8 @@ import { VocabularyState } from './vocabulary-state.interface';
 export class AppComponent implements OnInit, OnDestroy {
   vault$: Observable<WordVault>;
   vocabularyState$: Observable<VocabularyState>;
+  vocabularySubscription: Subscription;
   @ViewChild('form') form: NgForm;
-  // vaultSubscription: Subscription;
-  // rounds: { [key: string]: Round };
-  // vaultStateData: VocabularyState;
-  // vocabulaSubscription: Subscription;
 
   constructor(private appService: AppService) {}
 
@@ -38,10 +24,6 @@ export class AppComponent implements OnInit, OnDestroy {
     this.vault$ = this.appService.getWordVault();
     this.vocabularyState$ = this.appService.vocabularyState$;
     this.observeVocabularyStateChange();
-    // this.vault$.pipe(take(1)).subscribe(data => (this.rounds = data.rounds));
-    // this.vaultStateSubscription = this.vocabularyState$.subscribe(
-    //   state => (this.vaultStateData = state)
-    // );
   }
 
   onSubmit(
@@ -65,7 +47,7 @@ export class AppComponent implements OnInit, OnDestroy {
   }
 
   observeVocabularyStateChange() {
-    this.appService.indexOfQuestionJustAnswered$
+    this.vocabularySubscription = this.appService.indexOfQuestionJustAnswered$
       .pipe(
         withLatestFrom(this.vault$),
         tap(([indexOfQuestionJustAnswered, vault]) => {
@@ -86,7 +68,6 @@ export class AppComponent implements OnInit, OnDestroy {
     indexOfQuestionJustAnswered,
     vault
   ) {
-    console.log(vocabularyState);
     let newState: VocabularyState;
     const noOfRounds = Object.keys(vault.rounds).length;
     const noOfQuestionsInCurrentRound =
@@ -113,6 +94,6 @@ export class AppComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
-    // this.vaultStateSubscription.unsubscribe();
+    this.vocabularySubscription.unsubscribe();
   }
 }

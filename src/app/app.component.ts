86899f492509579fe63a16510ed7a9a405f89cad
anupkrbid <anupbid94@gmail.com>
2018-07-02 02:00:58 +0530

    Refactored Code & Added Comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -48,12 +48,16 @@ export class AppComponent implements OnInit, OnDestroy {
   }
 
   observeVocabularyStateChange() {
+    // this will capture all events when any questoin is answered correctly which will be a number
     this.vocabularySubscription = this.appService.indexOfQuestionJustAnswered$
       .pipe(
+        // this will get the laatest values from the word vault
         withLatestFrom(this.vault$),
+        // do something with the index of the answered question and the latest values form vault
         tap(([indexOfQuestionJustAnswered, vault]) => {
+          // get the last updated value of the vocabulary state
           const vocabularyState = this.appService.vocabularyState$.getValue();
-
+          // update the new vacabulary state dpending on the previous state
           this.handleVocabularStateChange(
             vocabularyState,
             indexOfQuestionJustAnswered,
@@ -90,6 +94,7 @@ export class AppComponent implements OnInit, OnDestroy {
     if (newState.currentRound > noOfRounds) {
       alert('Completed');
     } else {
+      // emiting event to update the vocabulary state with updated state
       this.appService.vocabularyState$.next(newState);
     }
   }
